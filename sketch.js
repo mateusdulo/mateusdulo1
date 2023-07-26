791c76754bc91a4986f756d5d625d4cacb68ea3a
mateusdulo <140652587+mateusdulo@users.noreply.github.com>
2023-07-26 17:02:30 -0300

    adicionando arquivo js e css

---
+++ sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+    createCanvas(600, 600);
+    background("white");
+  }
+  
+  function draw() {
+    
+   
+    stroke("pink")
+    fill("black");
+    
+    // console.log (mouseIsPressed)
+    
+    if(mouseIsPressed){
+      rect(mouseX, mouseY, 20, 30);
+    }
+    
+    
+    
+    
+  }
